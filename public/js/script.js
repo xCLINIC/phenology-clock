9a26b4bbbf47c3d450cb2e28d5726780cdf78ab0
blacki <blacki@gmail.com>
2014-11-20 23:37:14 -0500

    Made responsive and refactored prototype to reuable chart pattern.

--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,57 @@
 'use strict'
 function ƒ(str) { return function(obj) { return str ? obj[str] : obj; }}
 
-var margin = {top: 80, right:80, bottom: 80, left: 80},
-    w = Math.min(window.innerWidth, window.innerHeight) - margin.left - margin.right,
-    h = Math.min(window.innerWidth, window.innerHeight) - margin.top - margin.bottom,
-    d1 = new Date('1/1/2014'),
-    d2 = new Date('1/1/2015'),
-    time = d3.scale.linear().domain([d1,d2]).range([0,2*Math.PI])
+window.pheno = window.pheno || {}
 
-var tooltip = d3.select("body").append('div').classed('tooltip',true)
-  .style({
-    position:'absolute',
-    top:window.innerHeight/2-50+'px',
-    left:window.innerWidth/2-90+'px',
-    'text-align':'center',
-    opacity:1,
-    width: '200px',
-    height: '100px'
-  })
+pheno = function(parent,id){
+  var self = {},
+      chartId = id,
+      parentEl = parent,
+      data,
+      r,
+      svg,
+      cross,
+      cRadius,
+      tooltip,
+      arcPaths,
+      categories,
+      species = {},
+      arc = d3.svg.arc(),
+      c1 = d3.scale.category20b(),
+      c2 = d3.scale.category20c(),
+      d1 = new Date('1/1/2014'),
+      d2 = new Date('1/1/2015'),
+      margin = {r: 25, top: 25, right:25, bottom: 25, left: 25},
+      w = Math.min(window.innerWidth,window.innerHeight) - 2*margin.r,
+      h = Math.min(window.innerWidth,window.innerHeight) - 2*margin.r,
+      outerBound = Math.min(w,h),
+      innerBound = .08*outerBound,
+      time = d3.scale.linear().domain([d1,d2]).range([0,2*Math.PI])
 
-var svg = d3.select("body").append("svg")
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight)
+  self.draw = function() {
 
-var c1 = d3.scale.category20b()
-var c2 = d3.scale.category20c()
+    svg = d3.select(parentEl).html('').append("svg")
+      .attr("width", window.innerWidth)
+      .attr("height", window.innerHeight)
 
-d3.json("data.json", function(error, data) {
-
-  var r = d3.scale.linear().domain([0, data.length-1]).range([100, Math.min(w/2,h/2)])
-  var categories = _.uniq(data.map(function(d) {return d.category}))
-
-  var species = {}
-  _.each(categories, function(c) {
-    species[c] = _.filter(data, function(d,i) {
-      return c==d.category
+    r = d3.scale.linear().domain([0, data.length-1]).range([innerBound, Math.min(w/2,h/2)])
+    
+    categories = _.uniq(data.map(function(d) {return d.category}))
+    
+    _.each(categories, function(c) {
+      species[c] = _.filter(data, function(d,i) {
+        return c==d.category
+      })
     })
-  })
 
-  var arc = d3.svg.arc()
-    .innerRadius(function(d) {return parseInt(r(d.id))})
-    .outerRadius(function(d) {return parseInt(r(d.id)+Math.floor((r.range()[1]-r.range()[0])/data.length))})
-    .startAngle(function(d) { return time((new Date(d.start)).getTime()) })
-    .endAngle(function(d) { return time((new Date(d.end)).getTime()) })
+    arc.innerRadius(function(d) {return parseInt(r(d.id))})
+      .outerRadius(function(d) {return parseInt(r(d.id)+Math.floor((r.range()[1]-r.range()[0])/data.length))})
+      .startAngle(function(d) { return time((new Date(d.start)).getTime()) })
+      .endAngle(function(d) { return time((new Date(d.end)).getTime()) })
 
-  var cRadius = d3.scale.ordinal().domain(d3.range(categories.length)).rangePoints([0, w], 2);
+    cRadius = d3.scale.ordinal().domain(d3.range(categories.length)).rangePoints([0, w], 2)
 
-  var path = svg
+    arcPaths = svg
       .append("g")
         .classed('clock',true)
         .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
@@ -54,43 +59,82 @@ d3.json("data.json", function(error, data) {
         .data(data)
         .each(function(d,i) {d.id = i})
 
-  path.enter().append("path")
-    .style({
-      'fill-opacity': .7,
-      'stroke-width': '0px'
-    })
-    .attr('fill',function(d,i) {return (i<20) ? c1(i) : c2(i) })
-    .attr('stroke',function(d,i) {return (i<20) ? c1(i) : c2(i) })
-    .on('mouseover', function(d) {
-      tooltip
-        .attr('opacity',1)
-        .html('')
-        .append('h2')
-          .text(d['common-name'])
-        .append('p')
-          .text(d.category)
-      d3.select(this).style('fill-opacity', 1).attr('stroke-width', '3px')
-    })
-    .on('mouseout',function(d) {
-      tooltip
-        .attr('opacity',1)
-      d3.select(this).style('fill-opacity', .7).attr('stroke-width', '0px')
-    })
-    .transition()
-      .ease("elastic")
-      .duration(750)
-      .attr("d", arc);
+    arcPaths.enter().append("path")
+        .style({
+          'fill-opacity': .7,
+          'stroke-width': '0px'
+        })
+        .attr('fill',function(d,i) {return (i<20) ? c1(i) : c2(i) })
+        .attr('stroke',function(d,i) {return (i<20) ? c1(i) : c2(i) })
+        .on('mouseover', function(d) {
+          tooltip
+            .attr('opacity',1)
+            .html('')
+            .append('h2')
+              .text(d['common-name'])
+            .append('p')
+              .text(d.category)
+          d3.select(this).style('fill-opacity', 1).attr('stroke-width', '3px')
+        })
+        .on('mouseout',function(d) {
+          tooltip
+            .attr('opacity',1)
+          d3.select(this).style('fill-opacity', .7).attr('stroke-width', '0px')
+        })
+        .transition()
+          .ease("elastic")
+          .duration(750)
+          .attr("d", arc)
 
-  var cross = svg.append('g')
+    cross = svg.append('g')
         .classed('logo',true)
         .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
       .append('path')
-        .attr('d', d3.svg.symbol().type("cross").size(w*5))
+        .attr('d', d3.svg.symbol().type("cross").size(.05*outerBound*innerBound))
         .attr('transform','rotate(45)')
         .style({
           'fill':'rgb(236,47,45)',
           'fill-opacity': 1
         })
+
+    tooltip = d3.select(parentEl).append('div').classed('tooltip',true)
+      .style({
+        position:'absolute',
+        top:window.innerHeight/2-50+'px',
+        left:window.innerWidth/2-90+'px',
+        'text-align':'center',
+        opacity:1,
+        width: '200px',
+        height: '100px'
+      })
+
+  }
+
+  self.update = function(_d) {
+    data = _d
+    
+    if(w>0){ // should be minBound
+      self.draw(parentEl, chartId)
+    }
+  }
+
+  self.resize = function(_w,_h) {
+    margin = {r: 50, top: 25, right:25, bottom: 25, left: 25}
+    w = Math.min(window.innerWidth,window.innerHeight) - 2*margin.r
+    h = Math.min(window.innerWidth,window.innerHeight) - 2*margin.r
+    outerBound = Math.min(w,h)
+    innerBound = .08*outerBound
+    time = d3.scale.linear().domain([d1,d2]).range([0,2*Math.PI])
+
+    r = d3.scale.linear().domain([0, data.length-1]).range([innerBound, Math.min(w/2,h/2)])
+    cRadius = d3.scale.ordinal().domain(d3.range(categories.length)).rangePoints([0, w], 2)
+
+    
+    if(w>0){ // should be minBound
+      console.log('resize')
+      self.draw(parentEl, chartId)
+    }
+  }
 
   function arcTween(b) {
     var i = d3.interpolate({value: b.previous}, b);
@@ -98,7 +142,16 @@ d3.json("data.json", function(error, data) {
       return arc(i(t))
     }
   }
+  
+  return self
+}
+
+var clock = new pheno('body', 'clock')
+
+d3.json("data.json", function(error, data) {
+  clock.update(data)
+
   window.onresize = _.debounce(function(){
-    // console.log(window.innerWidth)
+    clock.resize()
   }, 250); 
 })
