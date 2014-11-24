b7439ef20df24f6aabb44e2354221fdc61eac7b9
blacki <blacki@gmail.com>
2014-11-24 00:22:59 -0500

    adde rotation, curved text, and clock effect

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,7 @@ pheno = function(parent,id){
       parentEl = parent,
       r,
       svg,
+      clock,
       data,
       cross,
       cRadius,
@@ -22,13 +23,14 @@ pheno = function(parent,id){
       d1 = new Date('1/1/2014'),
       d2 = new Date('1/1/2015'),
       doy = getDaysBetween(d1,d2),
-      c1 = d3.scale.category20b(),
-      c2 = d3.scale.category20c(),
+      c1 = d3.scale.category20c(),
+      c2 = d3.scale.category20b(),
       w = Math.min(window.innerWidth,window.innerHeight) - 3*margin.r,
       h = Math.min(window.innerWidth,window.innerHeight) - 3*margin.r,
       outerBound = Math.min(w,h),
-      innerBound = .10*outerBound,
+      innerBound = .15*outerBound,
       axisRadus = outerBound/2+10,
+      months = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ],
       time = d3.scale.linear().domain([d1,d2]).range([0,2*Math.PI])
 
   var line = d3.svg.line.radial()
@@ -40,56 +42,97 @@ pheno = function(parent,id){
       .attr("width", window.innerWidth)
       .attr("height", window.innerHeight)
 
-    var dayTicks = svg
+    var clock = svg.append("g").classed('clock',true)
+        .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
+
+    var dayTicks = clock
       .append("g")
         .classed('a-tick axis',true)
-        .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
       .selectAll("g")
         .data(d3.range(0, 365))
       .enter().append("g")
-        .attr("transform", function(d) { return "rotate(" + -time(doy[d])*180/Math.PI + ")"; })
+        .attr("transform", function(d) { return "rotate(" + (-time(doy[d])*180/Math.PI - 90) + ")"; })
     
     dayTicks.append("line")
-        .attr("x1", axisRadus)
+        .attr("x1", function(d) {
+          return (doy[364-d].getDate() == 1) ? 0:axisRadus
+        })
         .attr("x2", function(d) {
           return (doy[364-d].getDate() == 1 || doy[364-d].getDate()%7 ==0) ? axisRadus:axisRadus+10
         })
 
     dayTicks.append("text")
         .attr("x", axisRadus)
-        .attr("dy", ".35em")
+        .attr("dy", ".5em")
         .text(function(d) { return (doy[364-d].getDate() == 1 || doy[364-d].getDate()%7 ==0)? doy[364-d].getDate():''; })
-        // .style("text-anchor", function(d) { return d < 270 && d > 90 ? "end" : null; })
-        // .attr("transform", function(d) { return d < 270 && d > 90 ? "rotate(180 " + (axisRadus) + ",0)" : null; })
-        // .text(function(d) { return (doy[364-d].getDate() == 1 || doy[364-d].getDate()%7 ==0)? 364-d : "" })
+        // TODO fix this rotation
+        // .attr("transform", function(d) { 
+        //     if((doy[364-d].getDate() == 1 || doy[364-d].getDate()%7 ==0)) console.log(time(doy[364-d]))
+        //     return "rotate("+parseInt(time(doy[364-d])+Math.PI/2)+"," + (axisRadus) + ",0)"})
+        // .attr("transform", function(d) { 
+        //   var cos =  Math.cos(time(doy[364-d].getDate()))
+        //   var sin =  Math.sin(time(doy[364-d].getDate()))
+        //   var x =  Math.cos(time(doy[364-d].getDate()))
+        //   var y =  Math.sin(time(doy[364-d].getDate()))
 
-    var gr = svg.append("g")
+        //   console.log(d)
+        //   var matrix = "matrix(" +cos+ "," +sin+ "," +(-sin)+ "," +cos+ "," +x*(axisRadus)+ "," +y*(axisRadus)+ ")"
+        //   return (doy[364-d].getDate() == 1 || doy[364-d].getDate()%7 ==0)? matrix : null
+        // })
+
+
+    clock.append('g').classed('months', true)
+      
+    var monthTicks = d3.select('.months')
+      .selectAll('text')
+        .data(doy.filter(function(d){return d.getDate() == 1}))
+      .enter().append('text')
+
+    svg.append('defs')
+      .append("path")
+        .attr('d', function() {   
+          return "M 0 0 m -"+(axisRadus+15)+", 0 a "+(axisRadus+15)+","+(axisRadus+15)+" 0 1,1 "+(2*(axisRadus+15))+",0 a "+(axisRadus+15)+","+(axisRadus+15)+" 0 1,1 -"+(2*(axisRadus+15))+",0"
+        })
+        .attr('fill','none')
+        .attr('id', 'monthPath')
+        .attr('r', axisRadus+15)
+
+    monthTicks.append('textPath')
+      .attr('xlink:href','#monthPath')
+      .attr('startOffset',function(d,i) {return ((i+3)*100/12)%100+'%'})
+      .text(function(d,i) {return months[d.getMonth()]})
+      .on('click', function(d,i){
+        console.log(time(d))
+        d3.select('svg').style('transition-duration', '.8s')
+        orientSpeciesTop({startAngle:time(d)})
+      })
+
+    var categoryTicks = clock.append("g")
         .classed('r-tick axis',true)
-        .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
       .selectAll("g")
         .data(categories)
       .enter().append("g");
 
-    gr.append("circle")
-        .attr("r", function(d,i) {return cRadius(i)});
+    categoryTicks.append("circle")
+        .attr("r", function(d,i) {return cRadius(i)})
+        .attr('fill','none')
 
-    gr.append("text")
+    categoryTicks.append("text")
         .attr("y", function(d,i) { return -cRadius(i) - 4; })
         .attr("transform", "rotate(0)")
         .style("text-anchor", "middle")
         .text(function(d) { return d; });
 
-    arcPaths = svg
+    arcPaths = clock
       .append("g")
-        .classed('clock',true)
-        .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
+        .classed('arc',true)
       .selectAll("path")
         .data(data)
         .each(function(d,i) {d.id = i})
 
     arcPaths.enter().append("path")
         .style({
-          'fill-opacity': .7,
+          'opacity': .7,
           'stroke-width': '0px'
         })
         .attr('fill',function(d,i) {return (i<20) ? c1(i) : c2(i) })
@@ -102,14 +145,17 @@ pheno = function(parent,id){
               .text(d.data['common-name'])
             .append('p')
               .text(d.data.category)
-          d3.select(this).style('fill-opacity', 1).attr('stroke-width', '3px')
+          d3.select(this).style('opacity', 1)
         })
         .on('mouseout',function(d) {
           tooltip
             .attr('opacity',1)
-          d3.select(this).style('fill-opacity', .7).attr('stroke-width', '0px')
+          d3.select(this).style('opacity', .7)
         })
-        .on('click', orientSpeciesTop)
+        .on('click', function(d) {
+          d3.select('svg').style('transition-duration', '.8s')
+          orientSpeciesTop(d)
+        })
         .attr("d", arc)
         .transition()
         .duration(1000)
@@ -120,11 +166,10 @@ pheno = function(parent,id){
           }
         }))
 
-    cross = svg.append('g')
+    cross = clock.append('g')
         .classed('logo',true)
-        .attr("transform", "translate("+ window.innerWidth/2 + "," + window.innerHeight/2 + ")")
       .append('path')
-        .attr('d', d3.svg.symbol().type("cross").size(.05*outerBound*innerBound))
+        .attr('d', d3.svg.symbol().type("cross").size(.02*outerBound*innerBound))
         .attr('transform','rotate(45)')
         .style({
           'fill':'rgb(236,47,45)',
@@ -145,33 +190,34 @@ pheno = function(parent,id){
 
   self.init = function(_d) {
     data = _d
-
     r = d3.scale.linear().domain([0, data.length-1]).range([innerBound, Math.min(w/2,h/2)])
     arcDiff = Math.floor((r.range()[1]-r.range()[0])/data.length)
+    categories = _.uniq(data.map(function(d) {return d.category}))
+    
+    var temp = []
+    _.each(categories, function(c) {
+      species[c] = _.filter(data, function(d,i) {
+        return c==d.category
+      })
+      temp = temp.concat(species[c])
+    })
+    data = temp
     
     var temp = []
     _.each(_d,function(d,i) {
       var o = {}
       o.innerRadius = parseInt(r(i)-arcDiff)
       o.outerRadius = parseInt(r(i))
-      o.startAngle = time((new Date(d.start)).getTime())
+      o.startAngle = time((new Date(d.start)).getTime())+.01  //todo fix bad hack to add .01 offset 
       o.endAngle = time((new Date(d.end)).getTime())
       o.next = _.clone(o)
-      o.endAngle = time((new Date(d.start)).getTime())+.001 
+      o.endAngle = time((new Date(d.start)).getTime())
       o.data = d 
       temp.push(o)
     })
     data = temp
 
-    categories = _.uniq(data.map(function(d) {return d.data.category}))
-    
-    _.each(categories, function(c) {
-      species[c] = _.filter(data, function(d,i) {
-        return c==d.data.category
-      })
-    })
-    console.log(species)
-
+    console.log(data,categories)
     cRadius = d3.scale.ordinal().domain(d3.range(categories.length)).rangePoints([innerBound, outerBound/2+arcDiff], 2)
 
     if(w>0){ // should be minBound
@@ -183,65 +229,43 @@ pheno = function(parent,id){
     w = Math.min(window.innerWidth,window.innerHeight) - 3*margin.r
     h = Math.min(window.innerWidth,window.innerHeight) - 3*margin.r
     outerBound = Math.min(w,h)
-    innerBound = .08*outerBound
+    innerBound = .15*outerBound
     axisRadus = outerBound/2+10
     time = d3.scale.linear().domain([d1,d2]).range([0,2*Math.PI])
-
     r = d3.scale.linear().domain([0, data.length-1]).range([innerBound, Math.min(w/2,h/2)])
     arcDiff = Math.floor((r.range()[1]-r.range()[0])/data.length)
     cRadius = d3.scale.ordinal().domain(d3.range(categories.length)).rangePoints([0, outerBound/2], 2)
 
-    var species = []
     _.each(data,function(d,i) {
-
       d.innerRadius = parseInt(r(i)-arcDiff)
       d.outerRadius = parseInt(r(i))
       d.next.innerRadius = parseInt(r(i))
       d.next.outerRadius = parseInt(r(i)+Math.floor((r.range()[1]-r.range()[0])/data.length))
-
     })
-
 
     arcPaths.data(data)
     if(w>0){ // should be minBound
-      console.log('resize')
       self.draw(parentEl, chartId)
     }
   }
 
-  var rotateClock = function(angle) {
-    svg.attr('transform','rotate('+angle+')')
+  self.rotateClock = function(angle) {
+    d3.select('svg').style('transform','rotate('+angle+'deg)')
   }
 
-  // TODO rotate Axis along with Arcs
-  var orientSpeciesTop = function(p) {
-    var rotate = 2*Math.PI - p.startAngle
-    var _d = arcPaths.data()
-    var species = []
-    _.each(_d,function(d,i) {
-      var o = {}
-      d = d.data
-      o.innerRadius = parseInt(r(i)-arcDiff)
-      o.outerRadius = parseInt(r(i))
-      o.startAngle = time((new Date(d.start)).getTime())+ rotate
-      o.endAngle = time((new Date(d.end)).getTime())+ rotate
-      o.next = _.clone(o)
-      o.startAngle = time((new Date(d.start)).getTime())
-      o.endAngle = time((new Date(d.end)).getTime())
-      o.data = d 
-      species.push(o)
-    })
-    data = species
+  self.scheduleFn = function(fn){
+    function runFn(){
+      var t = new Date()
+      fn(d3.time.format("%b-%e-%H:%M")(t))
+      var s = t.getSeconds()
+      setTimeout(runFn, (80 - s)*1000)
+    }
+    runFn()
+  }
 
-    arcPaths.data(data)
-    .transition()
-    .duration(1000)
-    .attrTween("d", tweenArc(function(d, i) {
-      return {
-        startAngle: d.next.startAngle,
-        endAngle: d.next.endAngle
-      }
-    }))   
+  var orientSpeciesTop = function(p) {
+    var rotate = (p.startAngle<Math.PI) ? -(p.startAngle) : 2*Math.PI - p.startAngle
+    self.rotateClock(rotate*180/Math.PI)
   }
 
   var tweenArc = function (b) {
@@ -271,8 +295,17 @@ var clock = new pheno('body', 'clock')
 
 d3.json("data.json", function(error, data) {
   clock.init(data)
-
   window.onresize = _.debounce(function(){
     clock.resize()
   }, 250); 
 })
+
+var t = d3.scale.linear().domain([0,60000]).range([0,360])
+var deg =0
+var start = function (time) {
+  d3.select('svg').style('transition-duration', '61s')
+  deg +=180
+  clock.rotateClock(deg)
+}
+
+
